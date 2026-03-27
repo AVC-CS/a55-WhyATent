c6c0bd749d0e9f91acdf8cd33251d6d798fa2d41
WhyATent <WWyatnt@gmail.com>
2026-03-27 11:09:41 -0700

    a55 Done.

--- main.cpp
+++ main.cpp
@@ -14,17 +14,25 @@ int main()
 	cin >> M;
 
 	// TODO: declare variables for student name, score, sum, average
+	string sname;
+	double score, sum, average;
 
 	for(int i=0; i<N; i++)
 	{
 		// TODO: read student name and initialize sum
-
+		cout << "Student Name: " << endl;
+		cin >> sname;
 		for (int j=0; j<M; j++)
 		{
+			cout << "Enter score: " << endl;
+			cin >> score;
+			sum += score;
 			// TODO: read each score and accumulate sum
 		}
-
+		average = sum / M;
+		cout << sname << "\t" << sum << "\t" << average << endl;
 		// TODO: calculate average and print name, sum, average
+		sum = 0;
 	}
 
 	return 0;
